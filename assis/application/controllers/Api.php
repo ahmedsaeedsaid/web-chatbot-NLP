32f0a7c82bc743a5bb0e815a8ed39e0855407525
ahmedsaeedmedo <ahmedsaeedmedo6@gmail.com>
2019-07-13 19:47:33 +0200

    Create API requests mode

---
+++ assis/application/controllers/Api.php
@@ -0,0 +1,29 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$url = rtrim(__DIR__, 'controllers');
+require_once $url . 'classes/user.php';
+
+class Api extends CI_Controller {
+
+    private $data = NULL;
+
+    function __construct() {
+        parent::__construct();
+        // check if user has access permission
+        $this->load->library('authentication');
+        // loading models
+        $this->load->model('apiMod');
+    }
+
+    public function chatBotScript() {
+ 
+        
+    }
+    
+    
+
+    
+
+}
